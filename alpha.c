658576b2e32f7e07a4bfa0a6ed8f62e8b179d415
ManishaPriyadharshiniM <manisha96priya@gmail.com>
2016-07-08 11:30:40 +0530

    finish

---
+++ alpha.c
@@ -0,0 +1,15 @@
+# alpha
+#include<stdio.h>
+void main()
+{
+char c;
+printf("enter the character");
+scanf("%c",c);
+if(c=='a' && c=='z')||(c=='A' && c=='Z')
+{
+printf("it is an alphabet");
+else
+printf("it is not an alphabet");
+}
+return 0;
+}
